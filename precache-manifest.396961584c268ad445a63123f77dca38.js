7f1a751af1837e2a2f0a27d30fd4c83e24e0dd24
Jelena Manot <jelena.manot@tx.group>
2021-06-17 13:41:28 +0200

    Updates

---
+++ precache-manifest.396961584c268ad445a63123f77dca38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54aa75b6848c8e3e75aae6d8db5d1428",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b9affa623615ebf18752",
+    "url": "./static/css/main.80c7e5e0.chunk.css"
+  },
+  {
+    "revision": "70170f4af3e08384d86e",
+    "url": "./static/js/2.c20854b8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c20854b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9affa623615ebf18752",
+    "url": "./static/js/main.7dd428cb.chunk.js"
+  },
+  {
+    "revision": "be65fe249ea106456228",
+    "url": "./static/js/runtime-main.858138a9.js"
+  }
+]);
